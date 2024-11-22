5004a31a77f11f3f258c49870ae53d5e7e30af03
pediasprogramer <pedrodiasdesenvolvedor123@gmail.com>
2024-11-22 17:46:45 -0300

    Configuração do PostgreSQL com DATABASE_URL

--- config/db.js
+++ config/db.js
@@ -1,13 +1,14 @@
 const { Pool } = require('pg');
 
+// Configuração da pool de conexões usando a variável de ambiente DATABASE_URL
 const pool = new Pool({
-  user: 'postgres', // Substitua pelo usuário do banco
-  host: 'localhost', // Host do PostgreSQL
-  database: 'site_login', // Nome do banco de dados
-  password: '1234', // Substitua pela sua senha
-  port: 5432, // Porta padrão
+  connectionString: process.env.DATABASE_URL || 'postgresql://users_4t7z_user:DlUHjtOhLtrvaWm3bvGRCu7uqVQ3PiH8@dpg-ct0ent1u0jms73c4dslg-a/users_4t7z',
+  ssl: {
+    rejectUnauthorized: false, // Necessário para conexões seguras no Render
+  },
 });
 
+// Testa a conexão
 pool.query('SELECT NOW()', (err, res) => {
   if (err) {
     console.error('Erro ao conectar ao PostgreSQL:', err);
