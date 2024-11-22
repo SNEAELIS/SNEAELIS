2daf745b06447da8546983e103f1b265a1fbf829
pediasprogramer <pedrodiasdesenvolvedor123@gmail.com>
2024-11-22 17:15:09 -0300

    Adicionado suporte à variável PORT para o ambiente Render

--- src/app.js
+++ src/app.js
@@ -50,7 +50,7 @@ exports.cadastrarUsuario = async (req, res) => {
 };
 
 // Inicializa o servidor
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`Servidor rodando em http://localhost:${PORT}`);
+  console.log(`Servidor rodando na porta ${PORT}`);
 });
